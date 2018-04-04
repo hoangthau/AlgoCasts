cab14c9c9839df6266f4161fa76e0e43e259b13b
Thau Nguyen <thau.nguyen@orientsoftware.net>
2018-04-04 09:01:37 +0700

    Reverse a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let result = '';
+	if(!str || typeof str !== 'string') {
+		return result;
+	}
+	const len = str.length;
+	for(let i = len - 1; i >= 0 ; i--) {
+		result += str[i];
+	}
+	return result;
+}
 
 module.exports = reverse;
