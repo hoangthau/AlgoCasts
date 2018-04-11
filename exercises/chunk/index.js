dc80e4bf25d332abdf18cde9904ed4fba8b08021
Thau Nguyen <thau.nguyen@orientsoftware.net>
2018-04-11 09:08:31 +0700

    fizz buzz and chunk a array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let result = [];
+    let start = 0;
+    for( let [i, e] of array.entries()) {
+        const chunk = array.slice(start, start + size);
+        result.push(chunk);
+        start += size;
+        if(start > array.length -1){
+            break;
+        }
+    }
+    return result;
+}
 
 module.exports = chunk;
